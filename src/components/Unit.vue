707613cfbdc0835a50c27764610e3130a1409082
jordseow <jordanseow13@gmail.com>
2019-10-16 03:50:10 +0800

    Unit Pages without Code Editor & Output

--- src/components/Unit.vue
+++ src/components/Unit.vue
@@ -1,46 +1,108 @@
 <template>
-  <div class="main-wrapper">
-    <d-navbar toggleable="md" type="dark" theme="primary">
-      <d-container>
-      <d-navbar-toggle target="nav-collapse"></d-navbar-toggle>
-      <d-navbar-brand>
-          <h6 class="text-white my-auto mr-4">Shards Vue</h6>
-      </d-navbar-brand>
-      <d-collapse is-nav id="nav-collapse">
-        <d-navbar-nav>
-            <d-nav-item href="#">Home</d-nav-item>
-            <d-nav-item href="#">About</d-nav-item>
-            <d-nav-item href="#">Services</d-nav-item>
-        </d-navbar-nav>
-      </d-collapse>
-      <d-btn theme="white" size="sm" outline>Contact</d-btn>
-      </d-container>
-    </d-navbar>
-    <d-container class="py-4">
-      <h5 class="mb-4">Add New Post</h5>
-      <d-card class="w-100">
-        <d-card-body class="p-3">
-          <d-form>
-            <d-form-row>
-              <d-input class="mb-3" placeholder="Post title" />
-            </d-form-row>
-            <d-form-row>
-              <d-textarea class="mb-3" placeholder="Post content" :rows="6" />
-            </d-form-row>
-            <d-form-row class="justify-content-between">
-              <d-button size="sm" type="submit">Publish</d-button>
-              <d-button size="sm" outline theme="warning" class="ml-auto">Cancel</d-button>
-            </d-form-row>
-          </d-form>
-        </d-card-body>
-      </d-card>
-    </d-container>
-  </div>
-</template>
+  <d-container class="dr-example-container h-100">
+    <d-row class="pt-5">
+      <d-col>
+        <router-link to="/home">
+          <d-button size="lg" theme="warning" class="font-weight-bold">HOME</d-button>
+        </router-link>
+      </d-col>
+      <d-col>
+        <h1 class="text-white">CodeTranslate</h1>
+      </d-col>
+      <d-col class="text-right">
+        <popper
+          trigger="clickToToggle"
+          :options="{
+            placement: 'bottom',
+            modifiers: { offset: { offset: '0,10px' } }
+            }"
+        >
+          <div class="popper text-left">
+            <p class="font-weight-bold">To print in Python, we use</p>
+            <p>print(“your string here”)</p>
 
-<style>
-.main-wrapper {
-  background: #f9f9f9;
-  min-height: 100vh;
-}
-</style>
+            <p class="font-weight-bold">To print in Java, we use</p>
+            <p class="lead">System.out.println(“your string here”)</p>
+          </div>
+          <d-button size="lg" theme="info" class="font-weight-bold" slot="reference">DISCUSS</d-button>
+        </popper>
+      </d-col>
+    </d-row>
+    <d-row class="mt-5">
+      <d-col class="text-center" v-for="index in 5" :key="index">
+        <d-button block-level size="lg" theme="light">
+          <span class="font-weight-bold">Question {{ index }}</span>
+        </d-button>
+      </d-col>
+    </d-row>
+    <d-row class="mt-5">
+      <d-col>
+        <d-row>
+          <d-col>
+            <h5 class="text-white">From</h5>
+            <d-input-group class="mb-2">
+              <d-form-select v-model="translateFrom" :options="languages" />
+            </d-input-group>
+          </d-col>
+          <d-col>
+            <h5 class="text-white">To</h5>
+            <d-input-group class="mb-2">
+              <d-form-select v-model="translateTo" :options="languages" />
+            </d-input-group>
+          </d-col>
+        </d-row>
+
+        <d-row>
+          <d-col class="mt-5 pt-5">
+            <h4 class="text-white">Task</h4>
+            <h5 class="text-white">Print “Hello, world!”</h5>
+          </d-col>
+        </d-row>
+
+        <d-row class="mt-5 pt-5">
+          <d-col>
+            <popper
+              trigger="clickToToggle"
+              :options="{
+                        placement: 'top',
+                        modifiers: { offset: { offset: '0,10px' } }
+                        }"
+            >
+              <div class="popper text-left">
+                <p class="font-weight-bold">To print in Python, we use</p>
+                <p>print(“your string here”)</p>
+                <p class="font-weight-bold">To print in Java, we use</p>
+                <p class="lead">System.out.println(“your string here”)</p>
+              </div>
+              <d-button
+                size="lg"
+                theme="info"
+                class="font-weight-bold"
+                slot="reference"
+              >HINTS</d-button>
+            </popper>
+          </d-col>
+          <d-col>
+            <d-button size="lg" theme="info" class="font-weight-bold">RUN</d-button>
+          </d-col>
+        </d-row>
+      </d-col>
+    </d-row>
+  </d-container>
+</template>
+<script>
+import Popper from "vue-popperjs";
+import "vue-popperjs/dist/vue-popper.css";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    popper: Popper,
+  },
+  methods: {},
+  created() {
+    this.getUnit();
+  }
+};
+</script>
