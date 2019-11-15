4e1277d5f2d9b2713bc4839a9a723e88ffd665c2
SamuelLee <boonhowelee@gmail.com>
2019-11-15 23:41:52 +0800

    Update Lambda function

--- codeTranslateLambda/index.js
+++ codeTranslateLambda/index.js
@@ -1,18 +1,86 @@
 // This function uses the Node.js 10.x runtime
 // Remember to ENABLE CORS IN API GATEWAY
+function extractAllText(str){
+    const re = /"(.*?)"/g;
+    const result = [];
+    let current;
+    while (current = re.exec(str)) {
+        result.push(current.pop());
+        
+    }
+    return result.length > 0 ? result: [];
+}
 
 exports.handler = async (event) => {
     
     if (event.httpMethod === 'POST'){
         const parsedBodyContent = JSON.parse(event.body)
         const expectedSolution = parsedBodyContent["shown"]["0"]
-        const userSolution = parsedBodyContent["editable"]["0"]
+        const userSolutionNow = parsedBodyContent["editable"]["0"]
+        const hiddenCode = parsedBodyContent["hidden"]["0"]
+        let dialogTrue = hiddenCode.dialogCode.length >0
+        var userSolution = userSolutionNow
         let results = ""
-        let complete = ""
+        let complete = false
+        let printArray = []
+        let areEqual = false
         
+        if (hiddenCode.type == 'java') {
+            //extract printed statements
+            if (dialogTrue) {
+                printArray = extractAllText(userSolution)
+                
+                //check print statements
+                areEqual = printArray.length === hiddenCode.dialogCode.length && printArray.every((item,index) => hiddenCode.dialogCode[index] ===item)
+                
+            }
+            
+            //remove dialog from java code
+            userSolution = userSolution.replace(/".*?"/g,"")
+            //remove single line comment from java code
+            userSolution = userSolution.replace(/\/\/(.*)$/gm, "")
+            //remove spaces tabs and new lines
+            userSolution = userSolution.replace(/[ ]|\r\n|\n|\t|\r/gm, "")
+            //remove multi-line comments
+            userSolution = userSolution.replace(/\/\*.*\*\//g, "")
+            
+            //if there is printed statements
+            if (dialogTrue){
+                if (userSolution == expectedSolution && dialogTrue == areEqual){
+                    results = "You got the answer!"
+                    complete = true
+                }
+                //if the result is not correct but printed out right stuff
+                else if (userSolution.length != expectedSolution.length && dialogTrue == areEqual){
+                    results = "You are printing the right things but syntax is wrong."
+                }
+                else if (dialogTrue == true && areEqual == false){
+                    results = "You are not printing the right output."
+                }
+                else{
+                    results = "Please try again."
+                }
+            }
+            //if there are no printed statements
+            else if (userSolution == expectedSolution) {
+                results = "You got the answer!"
+                complete = true
+            }
+            //else if there is extra code written
+            else if (userSolution.length > expectedSolution.length) {
+                results = "You typed extra code."
+            }
+            //if there is too little code
+            else {
+                results = "You either did not make full user of the variables or am missing out some code."
+            }
+        
+        }
+        //if type is python
+        else {
         userSolution ===  expectedSolution ? (results = "You got the answer!") : results = "You have missed the answer."
-        userSolution ===  expectedSolution ? (complete = "true") : complete = "false"
-        
+        userSolution ===  expectedSolution ? (complete = true) : complete = false
+        }
         return {
             "statusCode": 200,
             "headers": {
@@ -33,10 +101,9 @@ exports.handler = async (event) => {
         return {
             "statusCode": 301,
             "headers": {
-                Location: 'http://dev-codetranslate797530.s3-website-eu-west-1.amazonaws.com/',
+                Location: "http://dev-codetranslate797530.s3-website-eu-west-1.amazonaws.com/",
             },
         }
     }
 
 };
-
